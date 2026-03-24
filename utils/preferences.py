f45bbc477a62fe14fb39a70f8c03de777853ae6c
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-24 08:34:38 +0800

    Update preferences.py

--- utils/preferences.py
+++ utils/preferences.py
@@ -1,31 +1,69 @@
-# utils/preferences.py
+# utils/__init__.py
 # -*- coding: utf-8 -*-
 """
 =============================================================================
 AMORIA - Virtual Human dengan Jiwa
-REDIRECT: Preferences classes are now in tracking/preferences.py
-This file is kept for backward compatibility only.
+Utils Package - Utilities, Logger, Exceptions, Helpers, Performance
 =============================================================================
 """
 
-import warnings
-
-warnings.warn(
-    "Importing from utils.preferences is deprecated. "
-    "Use 'from tracking.preferences import PreferencesLearner, PreferenceCategory, PreferenceItem' instead.",
-    DeprecationWarning,
-    stacklevel=2
+from .logger import setup_logging, get_logger, logger
+from .exceptions import (
+    AmoriaError, ConfigurationError, MissingAPIKeyError,
+    DatabaseError, ConnectionError, AINotAvailableError, AITimeoutError,
+    RegistrationNotFoundError, CharacterNotFoundError, SessionNotFoundError,
+    RoleNotFoundError, IntimacyLevelError, StaminaTooLowError,
+    ValidationError, InvalidCommandError, BackupError
 )
-
-# Redirect to tracking.preferences
-from tracking.preferences import (
-    PreferencesLearner,
-    PreferenceCategory,
-    PreferenceItem
+from .helpers import (
+    sanitize_input, truncate_text, extract_keywords, similarity_score,
+    format_number, format_duration, time_ago, calculate_age,
+    generate_temp_id, parse_command_args, validate_role, validate_intimacy_level,
+    random_percentage, random_choice_weighted, random_sentence,
+    get_local_greeting, get_local_affection, mix_local_language
 )
+from .performance import PerformanceMonitor, measure_time, async_measure_time, get_performance_monitor
 
 __all__ = [
-    'PreferencesLearner',
-    'PreferenceCategory',
-    'PreferenceItem'
+    'setup_logging',
+    'get_logger',
+    'logger',
+    'AmoriaError',
+    'ConfigurationError',
+    'MissingAPIKeyError',
+    'DatabaseError',
+    'ConnectionError',
+    'AINotAvailableError',
+    'AITimeoutError',
+    'RegistrationNotFoundError',
+    'CharacterNotFoundError',
+    'SessionNotFoundError',
+    'RoleNotFoundError',
+    'IntimacyLevelError',
+    'StaminaTooLowError',
+    'ValidationError',
+    'InvalidCommandError',
+    'BackupError',
+    'sanitize_input',
+    'truncate_text',
+    'extract_keywords',
+    'similarity_score',
+    'format_number',
+    'format_duration',
+    'time_ago',
+    'calculate_age',
+    'generate_temp_id',
+    'parse_command_args',
+    'validate_role',
+    'validate_intimacy_level',
+    'random_percentage',
+    'random_choice_weighted',
+    'random_sentence',
+    'get_local_greeting',
+    'get_local_affection',
+    'mix_local_language',
+    'PerformanceMonitor',
+    'measure_time',
+    'async_measure_time',
+    'get_performance_monitor',
 ]

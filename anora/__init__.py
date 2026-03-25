1d4e5494cf8506c809458cfe20ffa8af008ead9a
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-26 02:59:08 +0800

    Update __init__.py

--- anora/__init__.py
+++ anora/__init__.py
@@ -8,23 +8,25 @@ __version__ = "1.0.0"
 __author__ = "Nova"
 
 from .core import get_anora, anora
-from .database import get_anora_db, init_anora_db
+from .brain import get_anora_brain, anora_brain
+from .memory_persistent import get_anora_persistent
+from .roleplay_ai import get_anora_roleplay_ai
+from .roleplay_integration import get_anora_roleplay
+from .location_manager import get_anora_location, LocationType, LocationDetail
 from .chat import get_anora_chat
-from .intimacy import get_anora_intimacy
 from .roles import get_anora_roles
+from .intimacy import get_anora_intimacy
 from .places import get_anora_places
-from .thinking import get_anora_thought
-from .prompt import get_anora_prompt
-from .roleplay import get_anora_roleplay
 
 __all__ = [
     'get_anora', 'anora',
-    'get_anora_db', 'init_anora_db',
-    'get_anora_chat',
-    'get_anora_intimacy',
-    'get_anora_roles',
-    'get_anora_places',
-    'get_anora_thought',
-    'get_anora_prompt',
+    'get_anora_brain', 'anora_brain',
+    'get_anora_persistent',
+    'get_anora_roleplay_ai',
     'get_anora_roleplay',
+    'get_anora_location', 'LocationType', 'LocationDetail',
+    'get_anora_chat',
+    'get_anora_roles',
+    'get_anora_intimacy',
+    'get_anora_places',
 ]
